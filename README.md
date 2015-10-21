a8a1ba5c9d0ec8616e37e3585442127e39eb2482
Aleksandar Balalovski <abalalovski@gmail.com>
2015-10-21 10:19:17 +0200

    Moved to another repo

---
+++ README.md
@@ -0,0 +1,4 @@
+MultiTouchControllerTutorial
+============================
+
+We show how the Android Multi-touch controller (3rd party library) is implemented to make use of Bitmaps over a canvas to which drag/scale/rotate gestures can be applied. This repo is the example of the tutorial.
---
+++ src/com/twodwarfs/multitouchtut/MainActivity.java
@@ -0,0 +1,33 @@
+package com.twodwarfs.multitouchtut;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+
+/**
+ * MainAcitivy for the MultiTouch controller
+ * @author 2dwarfs.com
+ *
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        MultiTouchView multiTouchView = (MultiTouchView) findViewById(R.id.canvas_view);
+    
+        Bitmap itemBitmap = ((BitmapDrawable)getResources().getDrawable(R.drawable.logo)).getBitmap();
+        multiTouchView.setPinchWidget(itemBitmap);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
